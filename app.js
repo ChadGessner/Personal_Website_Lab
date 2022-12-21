c1f203dd81006334ae87518e5a5d6c32372623e6
ChadGessner <108926447+ChadGessner@users.noreply.github.com>
2022-12-21 10:37:24 -0500

    more changes

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ const home = "C:/Users/Chad/Desktop/Grand Circus/Personal_Website_Lab/home.html"
 const linksArray = [home,about,portfolio];
 const nav = document.getElementById('nav');
 let links = document.getElementsByClassName('link');
-
+const educationLinks = document.querySelectorAll('a.light-purple');
+const imageContent = document.getElementById('main-image');
+const imageTitle = document.getElementById('image-title')
 const loadLinks = () => {
     for(let i = 0; i < links.length;i++){
         links[i].href = linksArray[i];
     }
 }
+const onClick = () => {
+    educationLinks.forEach(l => {
+        l.addEventListener('click', ()=> {
+            imageTitle.innerText = l.title
+            imageContent.src = l.name;
+        })
+    })
+}
+onClick();
 loadLinks();
